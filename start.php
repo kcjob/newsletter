33df5ee73f2e9f8855a196d25bfaacc0a634d0fa
kcjob <30427093+kcjob@users.noreply.github.com>
2017-11-17 12:59:55 -0500

    Add files via upload

---
+++ start.php
@@ -0,0 +1,31 @@
+<?php
+require_once( __DIR__ . '/vendor/autoload.php');
+//use Symfony\Component\HttpFoundation\Request;
+
+use Spatie\PdfToImage\Pdf;
+use Spatie\Image\Image;
+use PdfToImage\PdfConversion;
+use PdfToImage\ImageManipulation;
+
+use \UserEmail\EmailSender;
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+// create a log channel
+$log = new Logger('database');
+$dbStream = new StreamHandler('log/newsletter.log', Logger::WARNING);
+$log->pushHandler($dbStream);
+
+$url = 'http://forum.sci.ccny.cuny.edu/administration/deans-office/publications/gazette/archives/fall-2017/vol3issue9.pdf';
+$path = 'documents/';
+$imageJpeg = 'newpdfimg.jpg';
+$width = '1000';
+$height = '800';
+
+//PDF CONVERSION TO IMAGE
+PdfConversion::convertToImage($url, $path.$imageJpeg);
+//IMAGE SIZE MANIPULATION
+ImageManipulation::changeImageDimensions($path.$imageJpeg, $width, $height);
+
+EmailSender::mailmsg();
