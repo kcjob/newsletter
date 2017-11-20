7ec3b98f4e658f249063a8b97f23837fefaee76f
kcjob <30427093+kcjob@users.noreply.github.com>
2017-11-20 15:30:20 -0500

    Update ImageManipulation.php

--- PdfToImage/ImageManipulation/ImageManipulation.php
+++ PdfToImage/ImageManipulation/ImageManipulation.php
@@ -3,29 +3,19 @@ namespace PdfToImage;
 use Spatie\Image\Image;
 
 class ImageManipulation {
-  static function changeImageDimensions($imageJpeg, $width, $height) {
+   static function changeImageDimensions($imageJpeg, $width, $height) {
 
-  $imgsize1 = getimagesize($imageJpeg);
-  echo 'ORIGINAL SIZE -- pixels -- COMMENT OUT FOR PRODUCTION';
-  var_dump($imgsize1);
+   echo 'ORIGINAL SIZE -- pixels -- COMMENT OUT FOR PRODUCTION';
+   $imgsize1 = getimagesize($imageJpeg);
+   var_dump($imgsize1);
 
-  $newImage =  Image::load($imageJpeg)
+   $newImage =  Image::load($imageJpeg)
                 ->width($width)
                 ->height($height)
                 ->save('documents/newimg.jpg');
 
-$imgsize = getimagesize('documents/newimg.jpg');
-
-echo 'RESIZED -- pixels -- COMMENT OUT FOR PRODUCTION';
-var_dump($imgsize);
-
-  /*$width = $newImage->getImageWidth(); // pixels
-  $height = $newImage->getImageHeight();
-  echo 'width: '.$width;
-  echo 'height: '. $height;*/
-
+   echo 'RESIZED -- pixels -- COMMENT OUT FOR PRODUCTION';
+   $imgsize = getimagesize('documents/newimg.jpg');
+   var_dump($imgsize);
   }
-
-
-
 }
